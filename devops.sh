f6ff51df1a2311b5ce95756a72878d265eaf18b8
afeyzidevops <a.feyzi.devops@gmail.com>
2025-06-28 10:29:34 +0330

    get parameters in shell script

--- devops.sh
+++ devops.sh
@@ -5,6 +5,27 @@ echo "Hello Devops engineers"
 name="DevOps_Course"
 
 echo $name
-echo ${name} --"best Practice"
+# best practice is :
+echo ${name}
 
 echo "Hi there $name"
+
+
+#You can also add variables in the Command Line outside the Bash script
+#and they can be read as parameters
+# ./devops.sh Ali Feizi
+
+# Pass frist parameter
+echo "Hello there" $1
+# pass second parameter
+echo "Hello there" $2
+#Pass  All Parameters
+echo  "Hello there" $@
+
+# Now let's go ahead and ask the user for input instead. 
+
+echo "whats is your name?"
+read name
+echo "Hi there $name"
+echo "Wellcom to DevOps Course"
+
